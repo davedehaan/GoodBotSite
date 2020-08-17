175f64381ca0fbda840862f7e6610ce6df8fdf1a
Dave DeHaan <david@redvanworkshop.com>
2020-08-17 15:06:55 -0400

    Updated styling on logs to make them more readable

--- resources/views/dashboard/logs.blade.php
+++ resources/views/dashboard/logs.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+
+<style>
+table.logs td:last-of-type {
+    text-align: left;
+}
+.aln-right {
+    text-align: right;
+}
+</style>
+
 <header class="special container">
     <span class="icon solid fa-running"></span>
     <h2>Logs: {{ $server->name }}</h2>
@@ -8,24 +18,24 @@
 <section class="wrapper style2 container special-alt">
     <div class="container">
         <div class="gb-row">
-            <table>
+            <table class="logs">
                 <tr>
                     <th width="50%">Event</th>
                     <th>User</th>
                     <th>Channel</th>
-                    <th>Time/Date</th>
+                    <th width="15%">Time/Date</th>
                 </tr>
                 @foreach ($logs AS $log)
                     <tr>
                         <td>{{ $log->event[0] }}</td>
-                        <td>{{ $log->event[1] }}</td>
-                        <td>{{ $log->event[2] }}</td>
+                        <td>{{ str_replace('Member: ', '', $log->event[1]) }}</td>
+                        <td>{{ str_replace('Channel: ', '', $log->event[2]) }}</td>
                         <td>{{ $log->createdAt }}</td>
                     </tr>
                 @endforeach
             </table>
         </div>
-        <div class="gb-row">
+        <div class="gb-row aln-right">
             @if ($page > 1) 
                 <a href="/dashboard/logs/{{ $server->id }}?p={{ $page - 1 }}">&larr; Previous</a> &nbsp;&nbsp;|&nbsp;&nbsp;
             @endif
